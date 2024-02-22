8b59447e2d0423b6506fa1ac82df07a471008274
Mark Peever <mark.peever@populi.co>
2024-02-22 14:43:46 -0800

    Update `#generate*` to use Mysql2 query instead of prepared statments
    Mysql2 prepared statements for casting, in order to use `{cast: false}` effectively,
    we need to runs queries directly instead of executing prepared statements.

--- mysql_connection.rb
+++ mysql_connection.rb
@@ -198,7 +198,7 @@ class MysqlConnection
   end
 
   def generate_delete(table_name, row_id)
-    statement = select_all_query(table_name)
+    statement = select_all_raw_query(table_name, row_id)
     if @database_name
       table_name = "#{@database_name}.#{table_name}"
     end
@@ -210,7 +210,7 @@ class MysqlConnection
     end
 
     @executor.execute(on_fail: fall_back) do
-      statement.execute(row_id, cast: false).each do |row|
+      @client.query(statement, cast: false).each do |row|
         wc = row.map do |k, v|
           if v.nil?
             %(#{k} IS NULL)
@@ -226,7 +226,7 @@ class MysqlConnection
   end
 
   def generate_insert(table_name, row_id)
-    statement = select_all_query(table_name)
+    statement = select_all_raw_query(table_name, row_id)
     if @database_name
       table_name = "#{@database_name}.#{table_name}"
     end
@@ -234,14 +234,14 @@ class MysqlConnection
     cmd = []
 
     fall_back = proc do |err|
-      @logger.error(%(caught error on table=>'#{table_name}', row_id => '#{row_id}', error: #{err}))
+      @logger.error(%(caught error on table=>'#{table_name}', row_id => '#{row_id}', error: #{err.inspect}))
       # and print it to STDERR too
-      warn(%(error on table=>'#{table_name}', row_id => '#{row_id}', error: #{err}))
+      warn(%(error on table=>'#{table_name}', row_id => '#{row_id}', error: #{err.inspect}))
       cmd
     end
 
     @executor.execute(on_fail: fall_back) do
-      statement.execute(row_id, cast: false).each do |row|
+      @client.query(statement, cast: false).each do |row|
         cols = []
         vals = []
         row.each do |k, v|
@@ -266,7 +266,7 @@ class MysqlConnection
   # TablePair::generate_update instead.
   def generate_update(table_name, row_id)
     primary_key = primary_key(table_name)
-    statement = select_all_query(table_name)
+    statement = select_all_raw_query(table_name, row_id)
     if @database_name
       table_name = "#{@database_name}.#{table_name}"
     end
@@ -279,7 +279,7 @@ class MysqlConnection
     end
 
     cmd = @executor.execute(on_fail: fall_back) do
-      statement.execute(row_id, cast: false).map do |row|
+      @client.query(statement, cast: false).map do |row|
         pairs = row.filter { |k, v| !k.eql?(primary_key) }.map do |k, v|
           val = if v.nil?
             "NULL"
@@ -416,11 +416,17 @@ class MysqlConnection
 
   memoize :row_checksum_query
 
+  def select_all_raw_query(table_name, row_id)
+    primary_key = primary_key(table_name)
+    %(select * from #{table_name} where #{primary_key} = '#{row_id}')
+  end
+
+  memoize :select_all_raw_query
+
   def select_all_query(table_name)
     primary_key = primary_key(table_name)
     query = %(select * from #{table_name} where #{primary_key} = ?)
     @client.prepare(query)
   end
-
   memoize :select_all_query
 end
